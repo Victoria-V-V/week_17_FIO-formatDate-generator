723102d4f8b733a344b71f7f368a0218d19a5641
Victoria-V-V <victoriavolcheck@yandex.ru>
2022-04-24 19:26:09 +0700

    Added hometask3 (generator)

--- index.js
+++ index.js
@@ -50,3 +50,41 @@ function formatDate() {
         }
     }, 200)
 };
+
+//Практическое задание №3
+const generatorButton = document.querySelector('.generator__button');
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+generatorButton.addEventListener("click", () => {
+    const generated = document.querySelector('.generated');
+    const minimum = document.querySelector('.minimum');
+    const maximum = document.querySelector('.maximum');
+    const average = document.querySelector('.average');
+    const sum = document.querySelector('.sum');
+    const product = document.querySelector('.product');
+
+    const generatedArray = [];
+    for (i = 0; i < 10; i++) {
+        generatedArray.push(getRandomIntInclusive(-10, 10));
+    }
+
+    let arraySum = 0;
+    let arrayProd = 1;
+    for (let i = 0; i < generatedArray.length; i++) {
+        arraySum += generatedArray[i];
+        arrayProd *= generatedArray[i];
+    }
+
+    generated.innerHTML = `Сгенерировали: ${generatedArray}`;
+    minimum.innerHTML = `Минимальное: ${Math.min(...generatedArray)}`;
+    maximum.innerHTML = `Максимальное: ${Math.max(...generatedArray)}`;
+    sum.innerHTML = `Сумма чисел: ${arraySum} `;
+    average.innerHTML = `Среднее арифметическое: ${arraySum/generatedArray.length} `;
+    product.innerHTML = `Произведение чисел: ${arrayProd}`;
+
+}, getRandomIntInclusive);
